114bbb86a7669dbdb39fe280bcfe180dbf7aad62
LamPhucNghi <lamphucnghi@gmail.com>
2018-10-10 17:41:21 +0700

    Final.1

--- LanguageModel/LanguageModel.py
+++ LanguageModel/LanguageModel.py
@@ -17,41 +17,38 @@ def main():
     word_word_freq = defaultdict(dict)
     
     with open(link_train_file+'input.pos','r',encoding='utf-8') as inputfile:
-        for line in inputfile:
-            line1 = line.lower()
+        for line in inputfile:                                                  #Duyet tung dong.
+            line1 = line.lower()                                                #Bo? viet hoa.
             
-            line_lowcase = line1.split()
+            line_lowcase = line1.split()                                        #Cat ca^u thanh list word.
             
-            curWord = line_lowcase[0]
+            curWord = line_lowcase[0]                                           #Xet word dau tien.
             preWord = 'None'
-            
+                                                                                #Dem word dau tien cua moi cau.
             if curWord not in word_freq:
                 word_freq[curWord] = 1
             else:
                 word_freq[curWord] += 1
-            
-            
+                                                                                #Duyet tiep tuc.
             for word in line_lowcase:
-                if preWord is 'None':
-                    curWord = word
-                    preWord = curWord
+                if preWord is 'None':                                           #Neu chua duyet tiep word thư 2.
+                    preWord = ''
                     pass
                 else:
                     preWord = curWord
-                    curWord = word                
-                
+                    curWord = word
+                                                                                #Dem tan suat word.
                     if word not in word_freq:
                         word_freq[word] = 1
                     else:
                         word_freq[word] += 1
-            
+                                                                                #Dem tan suat word_to_word.
                     if preWord not in word_word_freq:
                         word_word_freq[preWord][curWord] = 1
                     elif curWord not in word_word_freq[preWord]:
                         word_word_freq[preWord][curWord] = 1
                     else: word_word_freq[preWord][curWord] += 1
-                
-#            del word_word_freq[line_lowcase[0]]
+
     with open(link_out_file+'wordCount.txt','w',encoding='utf8') as outfile:
         json.dump(word_freq,outfile,ensure_ascii=False)
     display_out_file(word_word_freq,'word_to_word.txt')
