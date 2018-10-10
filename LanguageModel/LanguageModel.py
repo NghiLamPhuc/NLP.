dcbe6ef0748f691deb251d48504f7b7a3821d3fa
LamPhucNghi <lamphucnghi@gmail.com>
2018-10-10 18:54:26 +0700

    Thêm tính xác suất. Nhưng với bộ input 410 dòng. chạy 1p39s.

--- LanguageModel/LanguageModel.py
+++ LanguageModel/LanguageModel.py
@@ -1,6 +1,9 @@
 from collections import defaultdict
 import pprint
 import json
+from datetime import datetime
+start=datetime.now()
+
 
 link_folder_ = '\\Users\\NghiLam\\Documents\\GATSOP\\LanguageModel\\'
 link_out_file = link_folder_ + 'outfile\\'
@@ -16,7 +19,7 @@ def main():
     word_freq = dict() #count word.
     word_word_freq = defaultdict(dict)
     
-    with open(link_train_file+'input.pos','r',encoding='utf-8') as inputfile:
+    with open(link_train_file+'inputhalf.pos','r',encoding='utf-8') as inputfile:
         for line in inputfile:                                                  #Duyet tung dong.
             line1 = line.lower()                                                #Bo? viet hoa.
             
@@ -49,8 +52,26 @@ def main():
                         word_word_freq[preWord][curWord] = 1
                     else: word_word_freq[preWord][curWord] += 1
 
+# ghi file.
     with open(link_out_file+'wordCount.txt','w',encoding='utf8') as outfile:
         json.dump(word_freq,outfile,ensure_ascii=False)
-    display_out_file(word_word_freq,'word_to_word.txt')
+        
+    display_out_file(word_word_freq,'word_to_word_freq.txt')
+# Tinh xác suất.
+#    Smoothing
+    for eachWord in word_freq:
+        for thisWord in word_freq:
+            if thisWord not in word_word_freq[eachWord]:
+                word_word_freq[eachWord][thisWord] = 1
+    display_out_file(word_word_freq,'word_to_word_freq_after_add_one.txt')            
+# Xasc suat
+    v = len(word_freq)
+                
+    for eachWord in word_freq:
+        for thisWord in word_word_freq[eachWord]:
+            word_word_freq[eachWord][thisWord] = round((word_word_freq[eachWord][thisWord] + 1)/(word_freq[eachWord] + v),6)
+    
+    display_out_file(word_word_freq,'word_to_word_prob.txt')
     
 if __name__ == "__main__":main()
+print (datetime.now()-start)
