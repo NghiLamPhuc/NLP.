da936221cb4567e2f2d10f77774cbf59f120b341
LamPhucNghi <lamphucnghi@gmail.com>
2018-10-14 12:56:52 +0700

    2 hàm đọc file: read type textwrapper, read type string. 1 số comment.

--- Tokenize/python_split.py
+++ Tokenize/python_split.py
@@ -8,35 +8,39 @@ link_input_file = link_folder + 'input\\'
 link_output_file = link_folder + 'output\\'
 link_out_file = link_folder + 'outfile\\'
 
-#-----------------------------------------------------------------------------# Cac ham co ban.START
+#============================================================================== Một số hàm đọc ghi.
+#============================================================================== Ghi file ra từng dòng.
 def write_listline_file(List, filename, url):
     f = open(url + filename,'w',encoding='utf8')
     for t in List:
         f.write("%s\n" % t)
     f.close()
-    
+#============================================================================== Ghi 1 list.
 def write_file(file, filename, url):
     f = open(url + filename,'w',encoding='utf8')
     f.write(file)
     f.close()
-
-def read_file(url,filename):
+#============================================================================== Đọc file kiểu string.
+def read_file_stringtype(url,filename):
     f = open(url + filename,'r',encoding='utf8')
     input_file = f.read()
     f.close()
     return input_file
-#-----------------------------------------------------------------------------# Cac ham co ban.END
+#============================================================================== Đọc file kiểu Text Wrapper.
+def read_file_TextIOWrappertype(url, filename):                                 # Dùng for để duyệt qua từng phần tử.
+    return open(url + filename, 'r', encoding='utf8')
     
-#-----------------------------------------------------------------------------# Cac ham xu ly input.BEGIN
+#============================================================================== Loại bỏ một số dấu trng văn bản input.
 def remove_some(s):
     new_s = re.sub('<<\w\s','',s)
     new_s = re.sub('\/\w>>','',new_s)
     new_s = re.sub('\n+','\n',new_s)
+    new_s = re.sub('C. Ronaldo', 'C.Ronaldo', new_s)
 #    new_s = re.sub("[a-z][A-Z]\'\'",'"',new_s)                                 # 2 nháy đơn thành nháy kép.
 #    new_s = re.sub('\[\[','[',new_s)
 #    new_s = re.sub('\]\]',']',new_s)
     return new_s
-# The^m space va`o giua chu cai va ky tu dac biet
+#============================================================================== Thêm khoảng trắng vào ký tự.
 def add_space_word_matecharacter(s):
     return re.sub('(?<=\w)(?=[\/\_\-\(\)\{\}\[\]?!%\":;]\s)', ' ', s)
     
@@ -62,16 +66,15 @@ def add_space(s):
 #    regex12 = re.compile('(?<=[])(?=) | (?<=)(?=[])')                           #Dấu xấp xỉ ~
 #    regex13 = re.compile('(?<=[])(?=) | (?<=)(?=[])')                           #Dấu &
 
-############################ Tach cau ######################################### BEGIN
+#============================================================================== Tách câu.
 def split_sentences(url, filename):
-    s = read_file(url, filename)
+    s = read_file_stringtype(url, filename)
     regex = re.compile('(?<=[\.\?!])[\s\n]|\n')
     sentences = re.split(regex,s)
-
+    del sentences[-1]
     return sentences
-# Tách câu.#################################################################### END
 
-#################################### Tach tu to ################################ BEGIN
+#============================================================================== Tách từ tố.
 def tokenize(the_list):
     lst = list()
     
@@ -89,34 +92,32 @@ def tokenize(the_list):
         lst.append(i)
         
     return lst
+
+
 def tokenizez(url,filename):
     s = list()
-    sentence = read_file(link_output_file,filename)
-#    for sentence in list_sentences:
-    s = sentence.split()
-        
+    sentences = read_file_TextIOWrappertype(link_output_file,filename)
+    for sentence in sentences:
+        s.append(sentence.split())
     return s
-# Tách từ tố.################################################################## END
-
-#################################### Tach tu ################################# BEGIN
+#============================================================================== Tách từ.
 def word_seagment(the_list):
     
         
     return 0
-# Tách từ .#################################################################### END
 
-input_file = read_file(link_input_file,'12000-13000.txt')
-input_file = remove_some(input_file)
-write_file(input_file,'removed.txt',link_out_file)
-# ############# In kết quả tách câu ###########################################
-list_sentences = split_sentences(link_out_file,'removed.txt')
-write_listline_file(list_sentences,'sentences.txt',link_output_file)
-print ('So cau: %d' % len(list_sentences))
-# ############# In kết quả tách tu to #########################################
+#============================================================================== Main.
+#input_file = read_file_stringtype(link_input_file,'12000-13000.txt')
+#input_file = remove_some(input_file)
+#write_file(input_file,'removed.txt',link_out_file)
+#============================================================================== In kết quả tách câu.
+#list_sentences = split_sentences(link_out_file,'removed.txt')
+#write_listline_file(list_sentences,'sentences.txt',link_output_file)
+#print ('So cau: %d' % len(list_sentences))
+#============================================================================== In kết quả tách từ tố.
 tokens = tokenizez(link_output_file,'sentences.txt')
-print (tokens[0])
-write_listline_file(tokens,'tokens.txt',link_output_file)
-
-
+#for i in tokens:
+#    print (i)
+#write_listline_file(tokens,'tokens.txt',link_output_file)
 
 print (datetime.now()-start)
