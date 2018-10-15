b2bbc36dc17f76f213621520587cd735fc37010f
LamPhucNghi <lamphucnghi@gmail.com>
2018-10-15 17:30:50 +0700

    Sửa thành các def riêng.

--- LanguageModel/LanguageModel.py
+++ LanguageModel/LanguageModel.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 import pprint
 import json
+from collections import OrderedDict
 from datetime import datetime
 
 link_folder_ = '\\Users\\NghiLam\\Documents\\GATSOP\\LanguageModel\\'
@@ -8,18 +9,25 @@ link_out_file = link_folder_ + 'outfile\\'
 link_train_file = link_folder_ + 'input\\'
 link_result_file = link_folder_ + 'output\\'
 
+word_freq = dict() #count word.
+word_word_freq = defaultdict(dict)  #count word=>word
+word_word_prob = defaultdict(dict)  #calculate probability.
+word_prob_test = dict()             #sum of probability of one word.
+
 def display_out_file(lst,filename):
     file = link_out_file + filename
     with open(file, 'w', encoding='utf-8') as fout:
         pprint.pprint(lst,fout)
-        
-def main():
-    word_freq = dict() #count word.
-    word_word_freq = defaultdict(dict)  #count word=>word
-    word_word_prob = defaultdict(dict)  #calculate probability.
-    word_prob_test = dict()             #sum of probability of one word.
-    
-    with open(link_train_file+'input.pos','r',encoding='utf-8') as inputfile:
+
+# Đoán từ tiếp theo.
+def guess_next_word(word):
+#    lst = dict()
+    for item in word_word_prob[word]:
+        sorted_by_value = OrderedDict(sorted(word_word_prob[word].items(), key=lambda x: x[1]))
+    print (sorted_by_value)
+
+def training(filename):
+    with open(link_train_file + filename,'r',encoding='utf-8') as inputfile:
         for line in inputfile:                                                  #Duyet tung dong.
             line1 = line.lower()                                                #Bo? viet hoa.
             
@@ -54,12 +62,7 @@ def main():
                     else: word_word_freq[preWord][curWord] += 1
         
     display_out_file(word_word_freq,'word_to_word_freq.txt')                    #Ghi file tan suat word=>word
-# Tinh xác suất: những từ có trong train.
-    v = len(word_freq)                                                          #Số các từ trong train.
-    print (v)
     
-    #P(w_i | w_i-1) = count(w_i-1, w_i) / count(w_i-1)
-                
     for eachWord in word_freq:
         word_prob_test[eachWord] = 0
         for followWord in word_word_freq[eachWord]:
@@ -81,7 +84,74 @@ def main():
         outfile.write('\nWord count:\n')    
         for key,value in Lmodel['Word count'].items():
             outfile.write('%s:%s\n' % (key, value))
-
-start=datetime.now()
+    
+        
+def main():
+    start=datetime.now()
+    training('input.pos')
+    
+    
+    print (datetime.now()-start)
+#    with open(link_train_file+'input.pos','r',encoding='utf-8') as inputfile:
+#        for line in inputfile:                                                  #Duyet tung dong.
+#            line1 = line.lower()                                                #Bo? viet hoa.
+#            
+#            line_lowcase = line1.split()                                        #Cat ca^u thanh list word.
+#            line_lowcase.append('$end.')
+#            
+#            curWord = line_lowcase[0]                                           #Xet word dau tien.
+#            preWord = 'None'
+#                                                                                #Dem word dau tien cua moi cau.
+#            if curWord not in word_freq:
+#                word_freq[curWord] = 1
+#            else:
+#                word_freq[curWord] += 1
+#                                                                                #Duyet tiep tuc.
+#            for word in line_lowcase:
+#                if preWord is 'None':                                           #Neu chua duyet tiep word thư 2.
+#                    preWord = ''
+#                    pass
+#                else:
+#                    preWord = curWord
+#                    curWord = word
+#                                                                                #Dem tan suat word.
+#                    if word not in word_freq:
+#                        word_freq[word] = 1
+#                    else:
+#                        word_freq[word] += 1
+#                                                                                #Dem tan suat word_to_word.
+#                    if preWord not in word_word_freq:                           # Thiếu word => $.
+#                        word_word_freq[preWord][curWord] = 1
+#                    elif curWord not in word_word_freq[preWord]:
+#                        word_word_freq[preWord][curWord] = 1
+#                    else: word_word_freq[preWord][curWord] += 1
+#        
+#    display_out_file(word_word_freq,'word_to_word_freq.txt')                    #Ghi file tan suat word=>word
+# Tinh xác suất: những từ có trong train.
+#    v = len(word_freq)                                                          #Số các từ trong train.
+#    print (v)
+    
+    #P(w_i | w_i-1) = count(w_i-1, w_i) / count(w_i-1)
+                
+#    for eachWord in word_freq:
+#        word_prob_test[eachWord] = 0
+#        for followWord in word_word_freq[eachWord]:
+#            word_word_prob[eachWord][followWord] = word_word_freq[eachWord][followWord]/word_freq[eachWord]
+#            word_prob_test[eachWord] += word_word_prob[eachWord][followWord]
+#    
+#    display_out_file(word_prob_test,'test_probability.txt')
+    
+#    Lmodel = {}
+#    Lmodel['Language model'] = word_word_prob
+#    Lmodel['Word count'] = word_freq
+#    with open(link_result_file + 'model.txt', 'w',encoding='utf8') as outfile:
+#        json.dump(Lmodel, outfile, ensure_ascii=False)
+#    # GHI LAI CHO DE NHIN.
+#    with open(link_result_file + 'display_model.txt', 'w',encoding='utf8') as outfile:
+#        outfile.write('Language model:\n')
+#        for key,value in Lmodel['Language model'].items():
+#            outfile.write('%s:%s\n' % (key, value))
+#        outfile.write('\nWord count:\n')    
+#        for key,value in Lmodel['Word count'].items():
+#            outfile.write('%s:%s\n' % (key, value))
 if __name__ == "__main__":main()
-print (datetime.now()-start)
