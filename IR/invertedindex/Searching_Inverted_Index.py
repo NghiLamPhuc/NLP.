0551ab3d8092c1a6ce4f9890067394bfe1bc063a
lamphucnghi313 <lamphucnghi@gmail.com>
2018-12-06 10:52:23 +0700

    Thêm việc xử lý khi gặp ký tự dấu câu trong query. Có thể chạy qua nhiều document bằng tay. hihi.

--- IR/invertedindex/Searching_Inverted_Index.py
+++ IR/invertedindex/Searching_Inverted_Index.py
@@ -23,17 +23,13 @@ def main():
     f = open (link_output+'InvertedIndex.txt','r',encoding='utf-8-sig')
     inverted = json.load(f)
     print ('Có ' + str(len(inverted)) + ' tiếng.')
-# =========================== Đếm từ nào nhiều nhất ===========================
-    
-        
-    
 # ================================ stop words.=================================
     stop_words = get_stop_words()
 #    print (stop_words)
 # =============================== Câu cần tìm =================================
 #    query = 'rau an toàn'
-    query = 'rau an toàn chật vật tìm chỗ đứng'
-#    query = 'Cụ thể, các quan chức liên quan sẽ không được phép vào EU.'
+#    query = 'rau an toàn chật vật tìm chỗ đứng'
+#    query = 'Tôi từng thuộc danh sách những cầu thủ nòng cốt cho tới'
 #    query = 'Quốc, hội, châu, Âu.'
 #    query = 'Quốc hội châu'
 #    query = 'bệnh viện'
@@ -45,28 +41,45 @@ def main():
 #    query = 'các một'
 #    query = 'bệnh ăn'
 #    query = 'của bệnh'
-#    query = "kinh tế xhcn"
+#    query = "xhcn"
+#    query = 'văn hóa Việt Nam'
+#    query = 'khoa học kỹ thuật'
+    query = 'tai nạn giao thông'
     
 # ============================ Xử lý câu cần tìm ==============================    
+    regex1 = re.compile('(?<=[^\w+])(?=\w)')
+    regex2 = re.compile('(?<=\w)(?=[^\w+])')
+    
     query = query.lower()
-    query = re.sub('[^\w\s]','',query)
+#    query = re.sub('[^\w\s]','',query)
+    query = re.sub(regex1, ' ', query)
+    query = re.sub(regex2, ' ', query)
     words = query.split()
     
     print ("Cần Tìm: ")
+#    for i in words:
+#        if i.isalnum(): print ('1')
+#        else: print ('Khong phai tu')
     print (words)
     
-# =============================== Bắt đầu tìm =================================
+# ============================================================================= Bắt đầu tìm ==
     found_at_first_word = [] # Luu vi tri cua tu.
-    out_file = open(link_output+'output.txt','w',encoding='utf-8-sig') # Luu KET QUA CUOI CUNG.
+    in_dex = 1 #=======================================================================       index
+    doc_id = 'doc' + str(in_dex)
+    doc_name = 'removed '+ str(in_dex-1) +'.txt'
+    out_file = open(link_output+'output '+doc_id+'.txt','w',encoding='utf-8-sig') # Luu KET QUA CUOI CUNG.
+# Duyeejt qua tat ca document.
+#    for i in range(0,9):
+#        doc_id = 'doc'
+#        doc_id = doc_id+str(i+1)
 # =============================== Bắt đầu tìm =============== nếu query có 1 từ
     if len(words) == 1:
         if words[0] in stop_words:
             print ('Từ %s là stop word.' % words[0])
         else:
-            for pos in inverted[words[0]]['doc1']:
-                found_at_first_word.append(pos)
-            print ('Inverted index của từ "%s" có %d phần từ.' % (words[0],len(inverted[words[0]]['doc1'])))
-               
+            for pos in inverted[words[0]][doc_id]:
+               found_at_first_word.append(pos)
+
 # =============================== Bắt đầu tìm =============== nếu query có 2 từ
     if len(words) == 2:
 #        Nếu cả là stop word.
@@ -76,9 +89,9 @@ def main():
             else:
 #                Tìm vị trí từ thứ 2, trừ đi len(từ thứ 1) - 1
 #                Dò lại trong document vị trí mới đó.
-                temp = open(link_document+'removed 0.txt','r',encoding='utf-8-sig')
+                temp = open(link_document+doc_name,'r',encoding='utf-8-sig')
                 document_1 = temp.read()
-                for pos in inverted[words[1]]['doc1']:
+                for pos in inverted[words[1]][doc_id]:
                     pos_of_stop_word = pos - len(words[0]) -1
                     end_of_stop_word = pos - 2
 #                    print (end_of_stop_word)
@@ -90,9 +103,9 @@ def main():
                         found_at_first_word.append(pos)
                 temp.close()
         elif words[1] in stop_words:
-            temp = open(link_document+'removed 0.txt','r',encoding='utf-8-sig')
+            temp = open(link_document+doc_name,'r',encoding='utf-8-sig')
             document_1 = temp.read()
-            for pos in inverted[words[0]]['doc1']:
+            for pos in inverted[words[0]][doc_id]:
                 pos_of_stop_word = pos + len(words[0])
                 end_of_stop_word = pos_of_stop_word + len(words[1]) -1
                 words_1 = ''
@@ -102,21 +115,34 @@ def main():
                         found_at_first_word.append(pos)
             temp.close()
         else:
-            for pos_0 in inverted[words[0]]['doc1']:
+            for pos_0 in inverted[words[0]][doc_id]:
                 next_0 = pos_0 + len(words[0]) + 1
-                if next_0 in inverted[words[1]]['doc1']:
+                if next_0 in inverted[words[1]][doc_id]:
                     found_at_first_word.append(pos_0)
 # =============================== Bắt đầu tìm =================================
     if len(words) > 2:
-        for pos_0 in inverted[words[0]]['doc1']:
+#        Nếu từ đầu tiên là stop word
+#        if words[0] in stop_words:
+        for pos_0 in inverted[words[0]][doc_id]:
             next_0 = pos_0 + len(words[0]) + 1
             for i in range(1,len(words)):
-                if next_0 in inverted[words[i]]['doc1']:
-                    next_0 = next_0 + len(words[i]) + 1
-                else: break
-            if words[i] == words[-1]:
-                found_at_first_word.append(pos_0)
-# =============================== Ghi vi tri vao KET QUA CUOI CUNG ============
+#                Kieerm tra ky tu dac biet.
+                if words[i].isalnum():
+    #                Kiểm tra stop word
+                    if words[i] in stop_words:
+    #                while (words[i] in stop_words):
+                        next_0 = next_0 + len(words[i]) + 1
+                        continue
+                    else:
+                        if next_0 in inverted[words[i]][doc_id]:
+                            next_0 = next_0 + len(words[i]) + 1
+                        else: break
+                        if words[i] == words[-1]:
+                            found_at_first_word.append(pos_0)
+                else:# Neu words[i] la ky tu dac biet.
+                    next_0 = next_0 + 1
+                    
+# =============================== Ghi vi tri vao KET QUA CUOI CUNG =========================================
     if found_at_first_word:
         out_file.write('Found at: ')
         for i in found_at_first_word:
@@ -126,7 +152,7 @@ def main():
         print ('Không tìm thấy')
     
 # =============================== In câu chứa từ cần tìm  =====================
-    f1 = open(link_document+'removed 0.txt','r',encoding='utf-8-sig')
+    f1 = open(link_document+doc_name,'r',encoding='utf-8-sig')
     doc1 = f1.read()
     
     len_query = len(words)
