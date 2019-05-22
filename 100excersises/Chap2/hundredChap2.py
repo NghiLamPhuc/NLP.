2c73451dca2b380fbcc46c867a69df800fbe43cd
lamphucnghi313 <lamphucnghi@gmail.com>
2019-05-22 22:02:47 +0700

    no message

---
+++ 100excersises/Chap2/hundredChap2.py
@@ -0,0 +1,44 @@
+import os
+cwd = os.getcwd()
+#current working dir
+def read_File_Stringtype(url,filename):
+    f = open(url + filename,'r',encoding='utf8')
+    input_file = f.read()
+    f.close()
+    return input_file
+
+def write_Listline_File(List, filename, url):
+    f = open(filename,'w',encoding='utf8')
+    for t in List:
+        f.write("%s\n" % t)
+    f.close()
+
+f = read_File_Stringtype(cwd,"\\hightemp.txt")
+#print (f)
+
+#10
+def count_Line(f):
+    count = 0
+    for i in range(len(f)):
+        if f[i] == "\n":
+            count += 1
+    return f
+#wc -l hightemp.txt
+#11
+def tab_To_Space(f): return f.replace("\t"," ")
+#sed -e "s/\t/ /g" hightemp.txt
+
+def get_col_in_txt_file(strFile):
+    col1 = list()
+    col2 = list()
+    file = strFile.split('\n')
+    del file[-1]
+    for line in file:
+        lineSpt = line.split()
+        col1.append(lineSpt[0])
+        col2.append(lineSpt[1])
+    return col1,col2
+
+a,b=get_col_in_txt_file(f)
+write_Listline_File(a,'col1.txt',cwd+'\\Chap2')
+write_Listline_File(a,'col2.txt',cwd+'\\Chap2')
