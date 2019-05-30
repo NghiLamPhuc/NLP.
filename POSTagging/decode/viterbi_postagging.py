0f97f2d86a77972c5357bba4dc2f9a39a83a1057
lamphucnghi313 <lamphucnghi@gmail.com>
2019-05-30 12:42:25 +0700

    no message

--- POSTagging/decode/viterbi_postagging.py
+++ POSTagging/decode/viterbi_postagging.py
@@ -1,15 +1,20 @@
 import json
 import os
+
+#current folder this file
 cwd = os.getcwd()
+#get back folder of cwd 
 chdir = os.path.normpath(cwd + os.sep + os.pardir)
+#go to folder readwrite
+os.chdir(chdir + '\\readwrite\\')
+from readwrite import read_train_file, read_dictionary, write_dict, write_dict_two_type, write_Listline_File
+from create_Folder import createFolder
+#back to folder this file
+os.chdir(cwd)
 
 LINK_INPUT_FILE = cwd + '\\input\\'
 LINK_OUT_FILE = cwd + '\\outfile\\'
 LINK_OUTPUT = cwd + '\\output\\'
-
-def read_dictionary(url, fileName):
-    dictionary = json.load(open(url + fileName, encoding = 'utf-8'))
-    return dictionary
 
 #print (read_dictionary(chdir + '\\train\\outfile\\', 'transition_probability.txt'))
 #print (read_dictionary(chdir + '\\train\\outfile\\', 'emission_probability.txt'))
